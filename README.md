4affd0ed52d708a21c02ecff4eaf84310f63b987
abdushakoor12 <abdushakoor009@yahoo.com>
2024-10-10 23:45:29 +0500

    focus on link manager for now

--- README.md
+++ README.md
@@ -3,11 +3,10 @@
 An app for developers that has tools for developers.
 
 # Platforms
-- [] MacOS
-- [] Windows
-- [] Linux
+- [ ] MacOS
+- [ ] Windows
+- [x] Linux
 
 # Features
-- Screenshot maker (WIP)
 - Link Manager (WIP)
-- File converter, e.g. CSV to JSON, JSON to CSV etc (Later)
+- More....
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:dev_garage/features/screenshot/screenshot_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -17,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.amber),
         useMaterial3: true,
       ),
-      home: const ScreenshotMakerPage(),
+      home: const Scaffold(),
     );
   }
 }

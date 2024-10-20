1ad5d38178fcf441452eab6db3a748729626f8d0
abdushakoor12 <abdushakoor009@yahoo.com>
2024-10-20 12:22:53 +0500

    add shadnc

--- lib/features/link_manager/link_stores.dart
+++
@@ -1,8 +0,0 @@
-import 'package:dev_garage/features/link_manager/link_entity.dart';
-import 'package:loon/loon.dart';
-
-final linkEntityStore = Loon.collection<LinkEntity>(
-  "link_entities",
-  fromJson: LinkEntity.fromJson,
-  toJson: (entity) => entity.toJson(),
-);
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,10 @@
-import 'package:dev_garage/features/link_manager/link_manager_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:loon/loon.dart';
+import 'package:shadcn_ui/shadcn_ui.dart';
+
+final themeNotifier = ValueNotifier(ThemeMode.system);
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  Loon.configure(persistor: FilePersistor());
-
-  await Loon.hydrate();
 
   runApp(const MyApp());
 }
@@ -17,13 +14,45 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Dev Garage',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
-        useMaterial3: true,
-      ),
-      home: const LinkManagerScreen(),
+    return ValueListenableBuilder(
+      valueListenable: themeNotifier,
+      builder: (context, themeMode, child) {
+        return ShadApp.material(
+          title: 'Dev Garage',
+          debugShowCheckedModeBanner: false,
+          themeMode: themeMode,
+          home: Scaffold(
+            appBar: AppBar(
+              title: const Text("Dev Garage"),
+              actions: [
+                ConstrainedBox(
+                  constraints: BoxConstraints(maxWidth: 120),
+                  child: ShadSelect<ThemeMode>(
+                    placeholder: Text(themeMode.name),
+                    options: [
+                      ...ThemeMode.values.map((e) => ShadOption(
+                            value: e,
+                            child: Text(e.name),
+                          )),
+                    ],
+                    selectedOptionBuilder: (context, value) {
+                      return Text(value.name);
+                    },
+                    onChanged: (value) {
+                      themeNotifier.value = value;
+                    },
+                  ),
+                )
+              ],
+            ),
+            body: Center(
+              child: ShadButton(
+                child: Text("Hello"),
+              ),
+            ),
+          ),
+        );
+      },
     );
   }
 }

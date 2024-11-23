3a34620aea9532a4eba9109dd83790ac07ad7bbc
Abdul Shakoor <abdushakoor009@yahoo.com>
2024-11-23 19:34:18 +0500

    update widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,12 +1,8 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:dev_garage/core/db/app_database.dart';
 import 'package:dev_garage/core/locator.dart';
+import 'package:dev_garage/core/locator_root.dart';
+import 'package:dev_garage/features/home_screen.dart';
+import 'package:dev_garage/features/link_manager/link_manager_screen.dart';
 import 'package:dev_garage/main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -26,12 +22,25 @@ void main() {
     await db.close();
   });
 
+  testWidgets("Smoke Test", (tester) async {
+    await tester.pumpWidget(MyApp(locator: locator));
+    await tester.pumpAndSettle();
+
+    final homeScreenFinder = find.byType(HomeScreen);
+
+    expect(homeScreenFinder, findsOneWidget);
+  });
+
+  // this still fails for the dispose method in LinkManagerNotifier because of
+  // fakeAsyncTimer
   testWidgets('Notes get added into database', (WidgetTester tester) async {
     const testTitle = 'Test Title';
     const testUrl = 'https://www.google.com';
 
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp(locator: locator));
+    await tester.pumpWidget(LocatorRoot(
+        locator: locator, child: ShadApp.material(home: LinkManagerScreen())));
+
     await tester.pumpAndSettle();
 
     await tester.tap(find.text("Add Link"));
